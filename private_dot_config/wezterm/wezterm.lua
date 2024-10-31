4399948593809be9b19fcde2c94ce36309634b22
Zachary J. Rollyson <me@zjr.io>
2024-10-31 13:56:19 -0400

    why not have wider tabs?

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 
 -- Window config
-config.tab_max_width = 40
+config.tab_max_width = 80
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
