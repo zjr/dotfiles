5f0ad601bb68776d18274070b0f5065295fb9c2d
Zachary J. Rollyson <me@zjr.io>
2024-12-01 19:52:30 -0500

    fix: issues w/ autocmd w/o dir arg to nvim

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -2,17 +2,17 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- To remove & add padding when entering & exiting NVim, respectively.
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd("VimEnter", {
+-- Removes & add padding when entering & exiting NVim, respectively.
+-- See: https://github.com/wez/wezterm/issues/5561
+-- See RE LazyVimAutocmds: https://github.com/LazyVim/LazyVim/issues/2592
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimAutocmds",
   callback = function()
     --NVIM_ENTER=1
     vim.cmd([[call chansend(v:stderr, "\033]1337;SetUserVar=NVIM_ENTER=MQ==\007")]])
   end,
 })
-
-autocmd("VimLeavePre", {
+vim.api.nvim_create_autocmd("VimLeavePre", {
   callback = function()
     --NVIM_ENTER=0
     vim.cmd([[call chansend(v:stderr, "\033]1337;SetUserVar=NVIM_ENTER=MA==\007")]])
