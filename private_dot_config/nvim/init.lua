8bd384f0892d30d57a05b3c00200e89277c91e3f
Zachary J. Rollyson <me@zjr.io>
2024-12-16 17:17:32 -0500

    chore: add line to look for launch.json

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- tell dap to look for launch.json files
+require("dap.ext.vscode").load_launchjs(vim.fn.getcwd() .. "/.vscode/launch.json", {})
