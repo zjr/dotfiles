92ef3560e33edea4af204aee4c38fc3a2191ca01
Zachary J. Rollyson <me@zjr.io>
2024-12-17 13:51:08 -0500

    feat: more keymaps for zk, cd, search improve

--- private_dot_config/nvim/lua/plugins/zk.lua
+++ private_dot_config/nvim/lua/plugins/zk.lua
@@ -1,55 +1,44 @@
 return {
-  --{
-  -- "zk-org/zk-nvim",
-  -- config = function()
-  --   require("zk").setup({
-  --     picker = "telescope",
-  --   })
-  -- end,
-  -- keys = {
-  --   { "<leader>z", desc = "notes" },
-  --   -- Create a new note after asking for its title.
-  --   { "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "new" },
-  --   -- Open notes.
-  --   { "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "open" },
-  --   -- Open notes associated with the selected tags.
-  --   { "<leader>zt", "<Cmd>ZkTags<CR>", desc = "open with selected tags" },
-  --   -- Search for the notes matching a given query.
-  --   {
-  --     "<leader>zf",
-  --     "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
-  --     desc = "search for notes",
-  --   },
-  --   -- Search for the notes matching the current visual selection.
-  --   { "<leader>zf", ":'<,'>ZkMatch<CR>", desc = "serch for notes with selection" },
-  -- },
-  --},
-  -- {
-  --   "zk-org/neo-tree-zk.nvim",
-  --   dependencies = {
-  --     "nvim-neo-tree/neo-tree.nvim",
-  --     "zk-org/zk-nvim",
-  --   },
-  -- },
-  -- {
-  --   "nvim-neo-tree/neo-tree.nvim",
-  --   opts = {
-  --     sources = {
-  --       -- default sources
-  --       "filesystem",
-  --       "buffers",
-  --       "git_status",
-  --       -- user sources goes here
-  --       "zk",
-  --     },
-  --     zk = {
-  --       follow_current_file = true,
-  --       window = {
-  --         mappings = {
-  --           ["n"] = "change_query",
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "zk-org/zk-nvim",
+    lazy = false,
+    config = function()
+      require("zk").setup({
+        picker = "fzf_lua",
+      })
+    end,
+    keys = {
+      { "<leader>z", desc = "notes" },
+      -- Change to the notebook directory
+      { "<leader>zc", "<Cmd>ZkCd<CR>", desc = "cd to notebook" },
+      -- Create a new note after asking for its title
+      { "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "new note" },
+      -- Open notes
+      { "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "open note" },
+      -- Open notes associated with the selected tags
+      { "<leader>zt", "<Cmd>ZkTags<CR>", desc = "open with selected tags" },
+      -- Search for the notes matching a given query
+      {
+        "<leader>zf",
+        "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>",
+        desc = "search for notes",
+      },
+      -- Search for the notes matching the current visual selection
+      { "<leader>zf", mode = { "v" }, ":'<,'>ZkMatch<CR>", desc = "search for notes with selection" },
+    },
+    lsp = {
+      -- `config` is passed to `vim.lsp.start_client(config)`
+      config = {
+        cmd = { "zk", "lsp" },
+        name = "zk",
+        -- on_attach = ...
+        -- etc, see `:h vim.lsp.start_client()`
+      },
+      -- automatically attach buffers in a zk notebook that match the given filetypes
+      auto_attach = {
+        enabled = true,
+        filetypes = { "markdown" },
+      },
+    },
+  },
 }
