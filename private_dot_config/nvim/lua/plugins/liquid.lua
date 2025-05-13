a77d75a23c4292104931f42c33ff5e60dcd06036
Zachary J. Rollyson <me@zjr.io>
2025-05-12 20:57:09 -0400

    feat(nvim): conform formatting for liquid

---
+++ private_dot_config/nvim/lua/plugins/liquid.lua
@@ -0,0 +1,7 @@
+return {
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.liquid = { "prettier" }
+  end,
+}
