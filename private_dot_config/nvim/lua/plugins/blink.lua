aa64a687f745fcea01d0ef6576d10dfd44eb496e
Zachary J. Rollyson <me@zjr.io>
2025-09-02 09:43:00 -0400

    fix(nvim): super-tab issues with blink

--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -1,13 +1,39 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    keymap = { preset = "super-tab" },
+    keymap = {
+      preset = "none",
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+
+      ["<Tab>"] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    },
     signature = {
       enabled = true,
       window = { show_documentation = true },
     },
     completion = {
-      trigger = { show_in_snippet = false },
       list = {
         selection = {
           preselect = function()
