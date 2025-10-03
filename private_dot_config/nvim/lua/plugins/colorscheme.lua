633a8c1ba32d1a15778fb41a27fa7b04e282df24
Zachary J. Rollyson <me@zjr.io>
2025-10-03 10:42:57 -0400

    chore(nvim): bufferline issue fixed upstream

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,30 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			style = "storm",
-			-- transparent = true,
-			styles = {
-				-- sidebars = "transparent",
-				-- floats = "transparent",
-			},
-		},
-	},
-	{
-		"catppuccin",
-		opts = {
-			flavour = "frappe", -- latte, frappe, macchiato, mocha
-			background = { -- :h background
-				light = "latte",
-				dark = "frappe",
-			},
-			transparent_background = false,
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		},
-	},
-	{
-		"akinsho/bufferline.nvim",
-		init = function()
-			local bufline = require("catppuccin.groups.integrations.bufferline")
-			function bufline.get()
-				return bufline.get_theme()
-			end
-		end,
-	},
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "storm",
+      -- transparent = true,
+      styles = {
+        -- sidebars = "transparent",
+        -- floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin",
+    opts = {
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "frappe",
+      },
+      transparent_background = false,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
 }
