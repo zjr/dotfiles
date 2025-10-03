b3c9aa431bbbe4d195eedb13c00dd0ebb7f7e59f
Zachary J. Rollyson <me@zjr.io>
2025-10-03 11:31:27 -0400

    feat(nvim): trying transparent catppuccin

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -18,7 +18,11 @@ return {
         light = "latte",
         dark = "frappe",
       },
-      transparent_background = false,
+      transparent_background = true, -- disables setting the background color.
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
     },
   },
   {
