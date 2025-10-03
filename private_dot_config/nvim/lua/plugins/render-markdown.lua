48e1b75b82f42dd407b13c0b12ede5f97d44b4ca
Zachary J. Rollyson <me@zjr.io>
2025-10-03 10:43:49 -0400

    fix(nvim): render-markdown asking for moved dependency

--- private_dot_config/nvim/lua/plugins/render-markdown.lua
+++ private_dot_config/nvim/lua/plugins/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
 
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
 
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
