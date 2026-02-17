0c38cb3e8087310f61d91262ad1de1789d14b9a8
Zachary J. Rollyson <me@zjr.io>
2026-02-17 11:09:56 -0500

    fix(nvim): remove no longer needed launchjs from init

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- tell dap to look for launch.json files
-require("dap.ext.vscode").load_launchjs(vim.fn.getcwd() .. "/.vscode/launch.json", {})
