e5a432efc88ae0cf3054a1d687a26ba316539725
Zachary J. Rollyson <me@zjr.io>
2026-03-23 10:49:22 -0400

    feat(nvim): add a nicer go-impl plugin

--- private_dot_config/nvim/lua/plugins/go.lua
+++ private_dot_config/nvim/lua/plugins/go.lua
@@ -1,13 +1,37 @@
 return {
-  "olexsmir/gopher.nvim",
-  ft = "go",
-  -- branch = "develop", -- if you want develop branch
-  -- keep in mind, it might break everything
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    -- branch = "develop", -- if you want develop branch
+    -- keep in mind, it might break everything
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
+    },
+    ---@type gopher.Config
+    opts = {},
   },
-  ---@type gopher.Config
-  opts = {},
+  {
+    "fang2hou/go-impl.nvim",
+    ft = "go",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      -- Choose one of the following fuzzy finder
+      "folke/snacks.nvim",
+      -- "ibhagwan/fzf-lua",
+    },
+    opts = {},
+    keys = {
+      {
+        "<leader>ci",
+        function()
+          require("go-impl").open()
+        end,
+        mode = { "n" },
+        desc = "Go Impl",
+      },
+    },
+  },
 }
