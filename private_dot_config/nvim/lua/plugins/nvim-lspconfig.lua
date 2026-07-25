de1de1607461c80b2bf2012614f5be2410e76110
Zachary J. Rollyson <me@zjr.io>
2026-07-25 11:05:12 -0400

    chore: turn off inlay hints

--- private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,8 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = {
-      enabled = true,
-      exclude = { "go" }, -- filetypes for which you don't want to enable inlay hints
+      enabled = false,
     },
     servers = {
       gopls = {
