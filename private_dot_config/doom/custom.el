2250ec77a50a7e4cb634860d5f6d4a8e4048ddec
Zachary J. Rollyson <me@zjr.io>
2026-09-01 10:04:52 -0400

    feat(emacs): import doom config

---
+++ private_dot_config/doom/custom.el
@@ -0,0 +1,63 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(apheleia-mode-alist
+   '((markdown-mode . prettier-markdown) (lisp-data-mode . lisp-indent)
+     (lisp-interaction-mode . lisp-indent) (protobuf-mode . clang-format)
+     (cuda-ts-mode . clang-format) (cuda-mode . clang-format) (sh-mode . shfmt)
+     (asm-mode . asmfmt) (awk-mode . gawk) (bash-ts-mode . shfmt)
+     (bazel-mode . buildifier) (beancount-mode . bean-format)
+     (bibtex-mode . bibtex) (c++-ts-mode . clang-format)
+     (caddyfile-mode . caddyfmt) (cc-mode . clang-format)
+     (c-mode . clang-format) (c-ts-mode . clang-format)
+     (c++-mode . clang-format) (caml-mode . ocamlformat) (clojure-mode . cljfmt)
+     (clojure-ts-mode . cljfmt) (cmake-mode . cmake-format)
+     (cmake-ts-mode . cmake-format) (common-lisp-mode . lisp-indent)
+     (conf-toml-mode . taplo) (cperl-mode . perltidy)
+     (crystal-mode . crystal-tool-format) (csharp-mode . csharpier)
+     (csharp-ts-mode . csharpier) (css-mode . prettier-css)
+     (css-ts-mode . prettier-css) (dart-mode . dart-format)
+     (dart-ts-mode . dart-format) (dockerfile-mode . dprint)
+     (dockerfile-ts-mode . dprint) (elixir-mode . mix-format)
+     (elixir-ts-mode . mix-format) (elm-mode . elm-format)
+     (emacs-lisp-mode . lisp-indent) (fish-mode . fish-indent)
+     (gdscript-mode . gdformat) (gdscript-ts-mode . gdformat)
+     (gleam-ts-mode . gleam) (go-mode . gofmt) (go-ts-mode . gofmt)
+     (graphql-mode . prettier-graphql) (haskell-mode . fourmolu)
+     (haskell-ts-mode . fourmolu) (hcl-mode . hclfmt)
+     (html-mode . prettier-html) (html-ts-mode . prettier-html)
+     (hurl-mode . hurlfmt) (java-mode . google-java-format)
+     (java-ts-mode . google-java-format) (jinja2-mode)
+     (js3-mode . prettier-javascript) (js-json-mode . prettier-json)
+     (js-mode . prettier-javascript) (js-ts-mode . prettier-javascript)
+     (json-mode . prettier-json) (json-ts-mode . prettier-json)
+     (kotlin-mode . ktlint) (kotlin-ts-mode . ktlint) (latex-mode . latexindent)
+     (LaTeX-mode . latexindent) (lua-mode . stylua) (lua-ts-mode . stylua)
+     (lisp-mode . lisp-indent) (meson-mode . meson) (nasm-mode . asmfmt)
+     (neocaml-interface-mode . ocamlformat) (neocaml-mode . ocamlformat)
+     (nix-mode . nixfmt) (nix-ts-mode . nixfmt) (nomad-mode . nomad)
+     (objc-mode . clang-format) ("\\.mm\\'" . clang-format)
+     (perl-mode . perltidy) (php-mode . phpcs) (php-ts-mode . phpcs)
+     (pkl-mode . pkl) (purescript-mode . purs-tidy) (python-mode . black)
+     (python-ts-mode . black) (robot-mode . robotidy)
+     (ruby-mode . prettier-ruby) (ruby-ts-mode . prettier-ruby)
+     (rustic-mode . rustfmt) (rust-mode . rustfmt) (rust-ts-mode . rustfmt)
+     (snakemake-mode . snakefmt) (scss-mode . prettier-scss)
+     (sql-mode . pgformatter) (svelte-mode . prettier-svelte)
+     (terraform-mode . opentofu) (TeX-latex-mode . latexindent)
+     (TeX-mode . latexindent) (toml-ts-mode . taplo) (tsv-mode . yq-tsv)
+     (tsx-ts-mode . prettier-typescript) (tuareg-mode . ocamlformat)
+     (typescript-mode . prettier-typescript)
+     (typescript-ts-mode . prettier-typescript) (typst-mode . typstyle)
+     (typst-ts-mode . typstyle) (v-mode . vfmt) (web-mode . prettier)
+     (yaml-mode . prettier-yaml) (yaml-ts-mode . prettier-yaml)
+     (yang-mode . pyang) (zig-mode . zig-fmt) (zig-ts-mode . zig-fmt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
